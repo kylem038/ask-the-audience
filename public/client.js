748203f2efe9cf9dd22f310d8b588912e18f52ee
kylem038 <misenckj@gmail.com>
2016-12-21 11:27:28 -0700

    showing votes & alert message

--- public/client.js
+++ public/client.js
@@ -3,6 +3,8 @@ let socket = io();
 let connectionCount = document.getElementById('connection-count');
 let statusMessage = document.getElementById('status-message');
 let buttons = document.querySelectorAll('#choices button');
+let voteCount = document.querySelectorAll('#votes-count li');
+let voteCast = document.getElementById('vote-cast');
 
 
 socket.on('usersConnected', function (count) {
@@ -18,3 +20,14 @@ for (var i = 0; i < buttons.length; i++) {
     socket.send('voteCast', this.innerText);
   });
 }
+
+socket.on('voteSuccess', function (message) {
+  voteCast.innerText = 'Good job - you voted for ' + message;
+});
+
+socket.on('voteCount', function (votes) {
+  let array = Object.keys(votes);
+  for (var i = 0; i < voteCount.length; i++) {
+    voteCount[i].innerText = array[i] + ':' + votes[array[i]];
+  }
+});
