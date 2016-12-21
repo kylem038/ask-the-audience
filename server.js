4b9380c66bb4bb8858f988dc9dd802f6525d4893
kylem038 <misenckj@gmail.com>
2016-12-21 09:47:28 -0700

    server running

--- server.js
+++ server.js
@@ -8,3 +8,11 @@ app.use(express.static('public'));
 app.get('/', function (req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app)
+                 .listen(port, function () {
+                    console.log('Listening on port ' + port + '.');
+                  });
+module.exports = server;
