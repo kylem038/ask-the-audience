e00f6897b5c99a761ca308ff043b02409129b1ba
kylem038 <misenckj@gmail.com>
2016-12-21 09:55:11 -0700

    Listening to users coming & going

--- server.js
+++ server.js
@@ -12,7 +12,19 @@ app.get('/', function (req, res){
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app)
-                 .listen(port, function () {
-                    console.log('Listening on port ' + port + '.');
-                  });
+  .listen(port, function () {
+    console.log('Listening on port ' + port + '.');
+});
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+  });
+});
+
 module.exports = server;
