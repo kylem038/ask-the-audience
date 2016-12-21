0564f2802405921a7c2a6b0e2017aa970332e897
kylem038 <misenckj@gmail.com>
2016-12-21 09:31:40 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
